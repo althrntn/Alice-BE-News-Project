686839b7e2b18a5961f9bed03e068cfa44eabe0a
althrntn <alice.thornton89@gmail.com>
2022-09-08 11:34:47 +0100

    updating listen.js

--- db/listen.js
+++ db/listen.js
@@ -1,9 +1,7 @@
 const app = require("../app");
 
-const { application } = require("express");
-
 const { PORT = 9090 } = process.env;
-application.listen(PORT, (err) => {
+app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`listening on ${PORT}...`);
 });
