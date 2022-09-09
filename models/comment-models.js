d010d7d7b80279cea8fcee385cfe61c47dd03233
althrntn <alice.thornton89@gmail.com>
2022-09-09 12:47:10 +0100

    completing 18 patch-comment

--- models/comment-models.js
+++ models/comment-models.js
@@ -9,3 +9,26 @@ exports.removeComment = (comment_id) => {
       return results.rows[0];
     });
 };
+
+exports.updateCommentVotes = (comment_id, newVotes) => {
+  return db
+    .query("SELECT votes FROM comments WHERE comment_id = $1", [comment_id])
+    .then((result) => {
+      if (result.rows[0]) {
+        const votes = result.rows[0].votes;
+        const updatedVotes = votes + newVotes;
+        return updatedVotes;
+      } else {
+        return Promise.reject({ status: 404, msg: "comment not found" });
+      }
+    })
+    .then((updatedVotes) => {
+      return db.query(
+        "UPDATE comments SET votes = $2 WHERE comment_id = $1 RETURNING *;",
+        [comment_id, updatedVotes]
+      );
+    })
+    .then((result) => {
+      return result.rows[0];
+    });
+};
--- routers/api-router.js
+++ routers/api-router.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const { deleteComment } = require("../controllers/comment-controllers");
+const {
+  deleteComment,
+  patchCommentVotes,
+} = require("../controllers/comment-controllers");
 const { getTopics } = require("../controllers/topic-controller");
 const { getUsers, getUserById } = require("../controllers/user-controllers");
 const articlesRouter = require("./articles-router");
@@ -12,5 +15,6 @@ apiRouter.get("/users", getUsers);
 apiRouter.get("/users/:username", getUserById);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.delete("/comments/:comment_id", deleteComment);
+apiRouter.patch("/comments/:comment_id", patchCommentVotes);
 
 module.exports = apiRouter;
