10a64314cbbad9c08b2c303f838ea9df0ccf8650
althrntn <alice.thornton89@gmail.com>
2022-09-26 10:12:28 +0100

    correcting early invocation of cors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 
-app.use(cors());
-
 const apiRouter = require("./routers/api-router");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
